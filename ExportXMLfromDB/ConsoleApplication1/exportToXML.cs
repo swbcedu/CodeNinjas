59072b75ec47a1081e1b4293e6e1270aadb527b9
nskGIThubstar <nsampatk@hotmail.com>
2019-03-13 20:50:41 -0700

    XML work in progress

---
+++ ExportXMLfromDB/ConsoleApplication1/exportToXML.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class exportToXML
+    {
+        static void Main(string[] args)
+        {
+            string connetionString = null;
+            SqlConnection connection;
+            SqlDataAdapter adapter1;
+            SqlDataAdapter adapter2;
+            DataSet ds1 = new DataSet();
+            DataSet ds2 = new DataSet();
+            string sqlingredients = null;
+            string sqlrecipes = null;
+
+            var xmlfoldername = System.IO.Directory.GetCurrentDirectory();
+            var xmlIngredients = "Ingredients.xml";
+            var xmlRecipes = "Recipes.xml";
+            // **** UPDATE The connection string before you run.
+            connetionString = "Data Source='MININT-8862EOM\\SQLEXPRESS';Initial Catalog=RecipeOrganizer;Integrated Security=SSPI;";
+            connection = new SqlConnection(connetionString);
+            sqlingredients = "select * from Ingredients";
+            sqlrecipes = "select * from Recipes";
+
+            try
+            {
+                connection.Open();
+                adapter1 = new SqlDataAdapter(sqlingredients, connection);
+                adapter1.Fill(ds1, "Ingredients");                
+                ds1.WriteXml((Path.Combine(xmlfoldername, xmlIngredients)));
+
+                adapter2 = new SqlDataAdapter(sqlrecipes, connection);
+                adapter2.Fill(ds2, "Recipes");
+                ds2.WriteXml((Path.Combine(xmlfoldername, xmlRecipes)));
+
+                connection.Close();
+
+                Console.WriteLine("Done");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }            
+        }
+
+    }
+}
---
+++ ExportXMLfromDB/ExportXMLfromDB/ExportXMLfromDB/readWriteToXML.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ExportXMLfromDB
+{
+    public class readWriteToXML
+    {
+        // Created class to read data from DB and Export to XML
+        
+
+        static void Main()
+        {
+            RecipeOrganizerEntities roe = new RecipeOrganizerEntities();
+            /*
+            try {
+                roe.Database.Connection.Open();
+            }
+            catch (Exception e) { 
+            { Console.WriteLine(e.ToString()); }
+            }
+            */
+        }
+
+    }
+}
+
+

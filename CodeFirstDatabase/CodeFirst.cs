23a7d3aa42aff017a4adf73c8a0d47456f6e54b1
OlinBC <48326290+OlinBC@users.noreply.github.com>
2019-03-18 17:43:26 -0700

    Update CodeFirst.cs
    moved the using statments up to top of file - put comments into their own regions -- to clean things up a bit

--- CodeFirstDatabase/CodeFirst.cs
+++ CodeFirstDatabase/CodeFirst.cs
@@ -1,12 +1,13 @@
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Configuration;
+
 namespace CodeFirstDatabase
 {
-	using System;
-	using System.Data.Entity;
-	using System.Linq;
-    using System.Configuration;
-
 	public partial class CodeFirst : DbContext
 	{
+		#region comments
 		// Your context has been configured to use a 'CodeFirst' connection string from your application's 
 		// configuration file (App.config or Web.config). By default, this connection string targets the 
 		// 'CodeFirstDatabase.CodeFirst' database on your LocalDb instance. 
@@ -14,20 +15,22 @@ namespace CodeFirstDatabase
 		// If you wish to target a different database and/or database provider, modify the 'CodeFirst' 
 		// connection string in the application configuration file.
 		// the Application  has been modified to point to the local DB=- 
-
+		#endregion
 		public CodeFirst() : base("name=CodeFirst")
         {
-            Database.SetInitializer(new DropCreateDatabaseAlways<CodeFirst>());
+			Database.SetInitializer(new DropCreateDatabaseAlways<CodeFirst>());
         }
 
         public virtual DbSet<Ingredient> Ingredients { get; set; }
         public virtual DbSet<Recipe> Recipes { get; set; }
 
-        // Add a DbSet for each entity type that you want to include in your model. For more information 
-        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+		#region additional_comments
+		// Add a DbSet for each entity type that you want to include in your model. For more information 
+		// on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+		#endregion
 
-        // public virtual DbSet<MyEntity> MyEntities { get; set; }
-    }
+		// public virtual DbSet<MyEntity> MyEntities { get; set; }
+	}
 
 	//public class MyEntity
 	//{

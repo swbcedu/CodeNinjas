abba0c3fdbabda81fcae5296fd1216f0d764217e
triplethreatguy <stwaltemyer@gmail.com>
2019-03-27 15:58:03 -0700

    Search updates (all found)

--- Console/Program.cs
+++ Console/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
 using CodeFirstDatabase;
+using RecipeOrganizer;
 
 namespace Console
 {
@@ -12,37 +13,58 @@ namespace Console
     {
         static void Main(string[] args)
         {
-            int recsAdded = 0; 
+            List<string> searchSubject = new List<string>();
+            List<string> keywords = new List<string>();
 
-            using (CodeFirst db = new CodeFirst())
-            {
-                RecipeCF recipe = new RecipeCF
-                {
-                    Comment = "Fun Comment",
-                    Directions = "New Directions",
-                    RecipeType = "Object Recipe Type",
-                    ServingSize = "Serves Sizes XS-XL",
-                    Title = "Delicious Recipe",
-                    Yield = "Yield. No you yield!"
-                };
+            bool wasItFound = false;
+            List<int> foundIndexLocations = new List<int>();
 
-                IngredientCF ingredient = new IngredientCF()
-                {
-                    Description = "This is my cool description",
-                    RecipeID = 1
-                };
+            searchSubject.Add("The video is pretty mesmerizing on its own");
+            searchSubject.Add("And so far, it looks pretty slick");
+            searchSubject.Add("The Galaxy Fold is set to release on April 26th, starting at $1,980");
+            searchSubject.Add("I didn’t see a crease on those like in other leaked videos.");
 
-                db.Recipes.Add(recipe);
-                db.Ingredients.Add(ingredient);
-                recsAdded = db.SaveChanges();
+            keywords.Add("Video");
+            keywords.Add("Galaxy Fold");
+            keywords.Add("Galaxy");
+            keywords.Add("CrEasE");
+            keywords.Add("leaked");
 
-                RecipeCF rec = (from r in db.Recipes
-                              select r).FirstOrDefault();
+            Search.IsFound(keywords, searchSubject, out wasItFound, out foundIndexLocations);
 
-                System.Console.WriteLine(rec.Comment.ToString());
+            System.Console.ReadLine();
 
-                System.Console.ReadLine();
-            }
+            //int recsAdded = 0; 
+
+            //using (CodeFirst db = new CodeFirst())
+            //{
+            //    RecipeCF recipe = new RecipeCF
+            //    {
+            //        Comment = "Fun Comment",
+            //        Directions = "New Directions",
+            //        RecipeType = "Object Recipe Type",
+            //        ServingSize = "Serves Sizes XS-XL",
+            //        Title = "Delicious Recipe",
+            //        Yield = "Yield. No you yield!"
+            //    };
+
+            //    IngredientCF ingredient = new IngredientCF()
+            //    {
+            //        Description = "This is my cool description",
+            //        RecipeID = 1
+            //    };
+
+            //    db.Recipes.Add(recipe);
+            //    db.Ingredients.Add(ingredient);
+            //    recsAdded = db.SaveChanges();
+
+            //    RecipeCF rec = (from r in db.Recipes
+            //                  select r).FirstOrDefault();
+
+            //    System.Console.WriteLine(rec.Comment.ToString());
+
+            //    System.Console.ReadLine();
+            //}
         }
     }
 }
--- RecipeOrganizer/Search.cs
+++ RecipeOrganizer/Search.cs
@@ -16,14 +16,19 @@ namespace RecipeOrganizer
             List<int> indexList = new List<int>();
 
             // Tracking search terms that have been found. If empty, the search included all terms
-            List<string> termsNotFound = searchTerms;
+            List<string> termsNotFound = new List<string>();
+
+            foreach (string term in searchTerms)
+            {
+                termsNotFound.Add(term);
+            }
 
             int searchIndex = 0;
             foreach (string thing in toSearch)
             {
                 foreach (string term in searchTerms)
                 {
-                    if (thing.Contains(term))
+                    if (thing.ToLower().Contains(term.ToLower()))
                     {
                         // Store the index of the search object, only if it does not already exist in the indexList (so we only store it once)
                         if (!indexList.Contains(searchIndex))

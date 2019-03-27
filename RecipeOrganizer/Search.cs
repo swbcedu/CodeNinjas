f668b4a8ba31e9a2dcf49dfed81b1ec6bec6ef88
triplethreatguy <stwaltemyer@gmail.com>
2019-03-27 15:43:38 -0700

    Search update

--- RecipeOrganizer/Search.cs
+++ RecipeOrganizer/Search.cs
@@ -11,7 +11,12 @@ namespace RecipeOrganizer
         public static void IsFound(List<string> searchTerms, List<string> toSearch, out bool found, out List<int> foundIndex)
         {
             found = false;
+
+            // A list of index locations of positive search results
             List<int> indexList = new List<int>();
+
+            // Tracking search terms that have been found. If empty, the search included all terms
+            List<string> termsNotFound = searchTerms;
 
             int searchIndex = 0;
             foreach (string thing in toSearch)
@@ -20,16 +25,24 @@ namespace RecipeOrganizer
                 {
                     if (thing.Contains(term))
                     {
-                        found = true;
-
+                        // Store the index of the search object, only if it does not already exist in the indexList (so we only store it once)
                         if (!indexList.Contains(searchIndex))
                         {
                             indexList.Add(searchIndex);
                         }
+
+                        // Remove search term from termsNotFound
+                        termsNotFound.Remove(term);
                     }
                 }
 
                 searchIndex += 1;
+            }
+
+            // The search is only "successful" if all search keywords are found
+            if (termsNotFound.Count == 0)
+            {
+                found = true;
             }
 
             foundIndex = indexList;
